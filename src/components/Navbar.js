0de419dc263f5f6fa7f5760586db27a61c273e61
brysonwaisi <brysonnyamwange@gmail.com>
2022-11-16 13:45:28 +0300

    Fix Navbar to add Modal

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/metabnb-logo.svg"
+import Modal from "./Modal";
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false)
     return (
         <header>
             <div className="container row">
@@ -10,15 +13,18 @@ export default function Navbar() {
                 </a>
                 <nav className="navbar">
                     <ul className="nav__list nav__list--primary">
-                        <li><a href="#" className="nav__item">Home</a></li>
-                        <li><a href="#" className="nav__item">Place to Stay</a></li>
+                        <li><Link to="/" className="nav__item">Home</Link></li>
+
+                        <li><Link to="/placestostay" className="nav__item">Place to Stay</Link></li>
                         <li>
-                            <Link to="/places" className="nav__item">Places</Link>
+
+                            <a href="#" className="nav__item">Places</a>
                         </li>
                         <li><a href="#" className="nav__item">Community</a></li>
                     </ul>
                     <ul className="nav__list">
-                        <li><a href="#" className="nav__item btn-cta">Connect Wallet</a></li>
+                        <li><a href="#" className="nav__item btn-cta" onClick={() => setIsOpen(true)}>Connect Wallet</a></li>
+                        {isOpen && <Modal setIsOpen={setIsOpen} />}
                     </ul>
                 </nav>
             </div>
